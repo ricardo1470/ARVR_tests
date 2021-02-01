910c7a26457caf283bef03977b055a177e39a076
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-02-01 11:49:53 -0500

    create files

---
+++ firts_ARVR/Assets/scripts/XRbutton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class XRbutton : MonoBehaviour
+{
+    public void printMessage()
+    {
+        Debug.Log($"<color=green><b> make income! </b></color>");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
